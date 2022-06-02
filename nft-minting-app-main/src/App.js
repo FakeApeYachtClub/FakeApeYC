d7f6dfcfcb43aa510a278101030843a33322fa5b
FakeApeYachtClub <106606309+FakeApeYachtClub@users.noreply.github.com>
2022-06-02 10:30:26 -0400

    Add files via upload

--- nft-minting-app-main/src/App.js
+++ nft-minting-app-main/src/App.js
@@ -130,7 +130,7 @@ function App() {
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
     blockchain.smartContract.methods
-      .Mint(blockchain.account, mintAmount)
+      .Mint(mintAmount)
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
